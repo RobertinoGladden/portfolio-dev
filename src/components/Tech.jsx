0eddbaecab70c550261b0fd07cfd7e25d2de01a2
Robertino <gladdenrobertino@gmail.com>
2023-12-28 19:18:43 +0700

    initial commit

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,13 +5,13 @@ import { faJs, faReact, faVuejs, faPython, faHtml5, faCss3, faBootstrap } from "
 
 function Tech({ theme }) {
     const iconStyle = {
-        margin: '30px',
+        margin: '50px',
         transition: 'all 0.3s ease',
     };
 
     const iconHover = {
         transform: 'scale(1.1)',
-        boxShadow: theme === 'light' ? '0 0 10px rgba(255, 255, 255, 0.3)' : '0 0 10px rgba(0, 0, 0, 0.3)',
+        boxShadow: theme === 'light' ? '0 0 10px rgba(255, 255, 255, 0.6)' : '0 0 10px rgba(0, 0, 0, 0.6)',
     };
 
     return (
